895edc85a3f2da0f2bfffd08d4f73be6ae57fe0e
Kalliel <Kallielmpinheiro2004@gmail.com>
2024-09-16 09:35:22 -0300

    feat: login,cadastrar e index

--- project/PI3SEM/urls.py
+++ project/PI3SEM/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    
+    path('',include('user.urls')),
     path('admin/', admin.site.urls),
-    path('user/',include('user.urls'))
 ]
