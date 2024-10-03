51feb56f0b181dda01e1415abb9d63909df4d784
Kalliel <Kallielmpinheiro2004@gmail.com>
2024-10-03 09:29:56 -0300

    feat: marca rmentoria

--- project/scheduling/urls.py
+++ project/scheduling/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('horarios/<str:cpf>/',views.viewHorarios, name='viewHorarios')
+    path('horarios/<str:cpf>/',views.viewHorarios, name='viewHorarios'),
+    path('marcarMentoria/<str:cpf>/', views.marcarMentoria, name='marcarMentoria'),
 ]
--- project/scheduling/views.py
+++ project/scheduling/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from user.models import Perfil
+from django.shortcuts import render, get_object_or_404
+from user.models import Perfil, User
 from django.http import JsonResponse
 from mongoengine import DoesNotExist
 
@@ -12,3 +12,17 @@ def viewHorarios(request, cpf):
         return JsonResponse({'horariosDisponiveis': horariosDisponiveis}, safe=False)
     except DoesNotExist:
         return JsonResponse({'error': 'Perfil não encontrado'}, status=404)
+    
+def marcarMentoria(request, cpf):
+    if not request.user.is_authenticated:
+        return JsonResponse({'error': 'Usuário não autenticado.'}, status=401)    
+    if request.user.typeUser != 'mentorado':
+        return JsonResponse({'error': 'Apenas mentorados podem marcar mentorias.'}, status=403)
+    mentor = get_object_or_404(User, cpf=cpf, typeUser='mentor')
+    try:
+        perfil_mentor = Perfil.objects.get(cpf=mentor.cpf)
+    except Perfil.DoesNotExist:
+        return JsonResponse({'error': 'Perfil do mentor não encontrado.'}, status=404)
+    if not perfil_mentor.horariosDisponiveis:
+        return JsonResponse({'error': 'O mentor não tem horários disponíveis.'}, status=400)
+    return JsonResponse({'success': f'Mentoria marcada com o mentor {mentor.nome}.'})
