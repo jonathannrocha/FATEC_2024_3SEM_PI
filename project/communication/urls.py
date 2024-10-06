e902d609413990c99afd479b60f200554254067a
Kalliel <Kallielmpinheiro2004@gmail.com>
2024-10-06 13:40:24 -0300

    build: namespace organização

--- project/communication/urls.py
+++ project/communication/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import video_call
 
+app_name = 'videochamadas'
+
 urlpatterns = [
         path('video_call/', video_call, name='video_call'),
 ]
--- project/user/urls.py
+++ project/user/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from . import views
 
+app_name = 'user'
+
 urlpatterns = [
     path('',views.index, name='index'),
     path('cadastro/',views.cadastrarUsuario, name="cadastro"),
     path('login/',views.user_login, name='user_login'),
     path('logout/', views.logoutView, name='logout'),
-
-
+    path('chat/',views.chat, name='chat'),
+    path('dashboard/mentorprofile/', views.mentorProfile, name='mentorProfile'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('dashbord/conta', views.dashbord_conta, name= 'dashbord_conta')
 ]
